19c0a703da3dfd43c9c04f57609f3ce2d9c7c384
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-06-14 21:31:23 +0300

    style: update style with image

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,11 @@
+import image from '../../images/age_of_empires_467x181.jpeg';
+
 function Home() {
-  return <div></div>;
+  return (
+    <div className="main-padding">
+      <img src={image} alt="password show icon" />
+    </div>
+  );
 }
 
 export default Home;
