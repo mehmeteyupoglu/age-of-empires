619adbb4c47b01949548615243a982afafc32870
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-06-15 13:35:58 +0300

    update files

---
+++ src/__tests__/Home.test.js
@@ -0,0 +1,13 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import Home from '../pages/Home';
+
+afterEach(() => {
+  cleanup();
+});
+
+test('renders home page', () => {
+  render(<Home />);
+  const homePageElement = screen.getByTestId('home-page');
+  expect(homePageElement).toBeInTheDocument();
+  expect(homePageElement).toContainHTML('<div>');
+});
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import image from '../../images/age_of_empires_467x181.jpeg';
 
 function Home() {
   return (
-    <div className="main-padding centered-pages">
+    <div className="main-padding centered-pages" data-testid="home-page">
       <img src={image} alt="age-of-main-img" />
     </div>
   );
