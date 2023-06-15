5b31b3022686eb4efc8f4606457537eeedbb289c
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-06-15 15:03:32 +0300

    fix: update component contents

--- src/pages/Units/index.js
+++ src/pages/Units/index.js
@@ -1,5 +1,37 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import CustomButton from '../../components/atoms/Button';
+import { useNavigate } from 'react-router-dom';
+import { getUnitsRequest } from '../../store/actions/unitsActions';
+
 function Units() {
-  return <div className="todos centered-pages"></div>;
+  const state = useSelector((state) => state);
+  const { unitsReducer } = state;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
+  const listTodos = () => {
+    dispatch(getUnitsRequest());
+  };
+
+  return (
+    <div className="todos centered-pages">
+      <div>
+        <CustomButton title={'Back'} onClick={handleClick} loading={unitsReducer.isLoading} />
+        <CustomButton
+          title={'List Units'}
+          onClick={listTodos}
+          loading={unitsReducer.isLoading}
+          async
+        />
+        <ToastContainer />
+      </div>
+    </div>
+  );
 }
 
 export default Units;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import Todos from '../pages/Todos';
-import Units from '../pages/Todos';
+import Units from '../pages/Units';
 
 export { Home, NotFound, Todos, Units };
