4f01120ed07b04b79771e88a3c9840aa71dd7c19
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-06-15 14:54:13 +0300

    fix: fix units and todos pages

--- src/pages/Units/index.js
+++ src/pages/Units/index.js
@@ -1,41 +1,5 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { ToastContainer } from 'react-toastify';
-
-import CustomButton from '../../components/atoms/Button';
-import { useNavigate } from 'react-router-dom';
-
-import './Todos.scss';
-import { getUnitsRequest } from '../../store/actions/unitsActions';
-
-function Todos() {
-  const state = useSelector((state) => state);
-  const { unitsReducer } = state;
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate('/');
-  };
-
-  const listTodos = () => {
-    dispatch(getUnitsRequest());
-  };
-
-  return (
-    <div className="todos centered-pages">
-      <h1>Todos</h1>
-      <div>
-        <CustomButton title={'Back'} onClick={handleClick} loading={unitsReducer.isLoading} />
-        <CustomButton
-          title={'List Todos'}
-          onClick={listTodos}
-          loading={unitsReducer.isLoading}
-          async
-        />
-        <ToastContainer />
-      </div>
-    </div>
-  );
+function Units() {
+  return <div className="todos centered-pages"></div>;
 }
 
-export default Todos;
+export default Units;
