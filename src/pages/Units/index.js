85c37ee2a370a54688d02552dcfcf7b81f6715c2
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-06-16 17:26:56 +0300

    feat: initiate filter by cost functionality

--- src/pages/Units/index.js
+++ src/pages/Units/index.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import Form from 'react-bootstrap/Form';
+
 import { useDispatch } from 'react-redux';
 import { getUnitsRequest } from '../../store/actions/unitsActions';
 import CustomTable from './CustomTable';
@@ -11,6 +14,8 @@ function Units() {
     type: 'age',
     value: 'All'
   });
+  const [checked, setChecked] = React.useState(true);
+  const [value, setValue] = React.useState([20, 37]);
   const dispatch = useDispatch();
 
   React.useEffect(() => {
@@ -29,6 +34,14 @@ function Units() {
     });
   };
 
+  const handleCost = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChecked = (event) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <>
       <Box sx={{ width: '100%' }} className="center-layout">
@@ -41,6 +54,31 @@ function Units() {
           <Tab value="Feudal" label="Feudal" />
           <Tab value="Castle" label="Castle" />
         </Tabs>
+      </Box>
+      <Box sx={{ width: '100%' }} className="center-layout">
+        <span>
+          <pre>Filter by cost</pre>
+        </span>
+        <div className="filter-group">
+          <Form.Check
+            type={'checkbox'}
+            id={`default-checkbox`}
+            label={`Wood`}
+            onChange={handleChecked}
+            checked={checked}
+          />
+          <Slider
+            getAriaLabel={() => 'Cost Range'}
+            value={value}
+            onChange={handleCost}
+            valueLabelDisplay="auto"
+            min={0}
+            max={200}
+            step={10}
+            disabled={!checked}
+          />
+          {checked ? <span>{`${value[0]} - ${value[1]}`}</span> : <span>--</span>}
+        </div>
       </Box>
       <CustomTable />
     </>
