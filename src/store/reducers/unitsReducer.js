6122742df50f34c421a7ba8ecc4332792bc87a50
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-06-17 13:31:46 +0300

    chore: remove logs

--- src/store/reducers/unitsReducer.js
+++ src/store/reducers/unitsReducer.js
@@ -6,13 +6,11 @@ const initialState = {
 export const unitsReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'GET_UNITS_REQUEST':
-      console.log('request works well');
       return {
         ...state,
         isLoading: true
       };
     case 'GET_UNITS_REQUEST_SUCCESS':
-      console.log('success');
       return {
         ...state,
         units: action.payload,
