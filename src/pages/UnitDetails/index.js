45904c53f126153858cce35cbba3d86a998b3c84
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-06-18 12:26:28 +0300

    feat: initiate unit details functionality

---
+++ src/pages/UnitDetails/index.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { useEffect } from 'react';
+import Box from '@mui/material/Box';
+import { useParams } from 'react-router-dom';
+
+import { useDispatch } from 'react-redux';
+import { getUnitsRequest } from '../../store/actions/unitsActions';
+
+function UnitDetails() {
+  const dispatch = useDispatch();
+  const params = useParams();
+
+  useEffect(() => {
+    console.log('params', params);
+    dispatch(getUnitsRequest(`?id=${params.id}`));
+  }, []);
+
+  return (
+    <>
+      <Box sx={{ width: '100%' }} className="center-layout">
+        <span>
+          <pre>Filter by cost</pre>
+        </span>
+      </Box>
+    </>
+  );
+}
+
+export default UnitDetails;
